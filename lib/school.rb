02d41ed2a2836dd907e5ba936c621e8b141e511b
alfredwang0707 <alfredwang0707@gmail.com>
2020-10-27 18:39:29 -0400

    commit tree 5a69b52ae5aeab24bc37b0bbdc4780bb545454e3

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+require "pry"
+class School
+    attr_accessor :roster
+    def initialize(roster)
+    @roster = {}
+    end
+
+    def add_student(name,grade)
+        roster[grade] ||= []
+        roster[grade] << name
+        #binding.pry
+        #In short, a||=b means: 
+        #If a is undefined, nil or false, 
+        #assign b to a. Otherwise, keep a intact.
+    end
+
+    def grade(grade)
+        roster[grade]
+    #binding.pry    
+    end
+
+    def sort 
+        roster.each do |grade, name|
+        #binding.pry     
+        name.sort!
+        end
+        #sorted = change.sort # Ascending sort
+        #sorted = change.sort { |a, b| a <=> b } # Same thing!
+        
+    end
+
+end
